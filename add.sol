cac9b8b7c0fd75df0be122f8be3a957ebc3d2b05
vindhya114 <vindhyamurali01@gmail.com>
2024-09-22 15:36:36 +0530

    Create add.sol

---
+++ add.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+contract Addition {
+    uint8 x;
+
+    function addx(uint8 y, uint8 z) public {
+        x = y + z;
+    }
+
+    function retrievex() public view returns (uint8) {
+        return x;
+    }
+}
